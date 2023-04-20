c47e4c9b33b2962906a25c7f5c5b36744b2e44c9
Roshani-Chaurasiya <98791128+RoshaniChaurasiya@users.noreply.github.com>
2023-04-20 14:40:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import './App.css'; 
+import TextAnimationApp from './project/text-animation/TextAnimationApp';
+// import ResponsiveNav from './project/responsive-nav/ResponsiveNav';
+// import NotesApp from './project/notes-app/NotesApp';
+// import RegForm from './project/form-validation/RegForm';
+// import FilterContact from './project/filter-contact-app/FilterContact';
+// import DialogBox from './project/dialog-box/DialogBox';
+// import ProgressBarComp from './project/progressBar/ProgressBarComp';
+// import PopupApp from './project/magicPopup/PopupApp';
+// import SliderApp from './project/slider/SliderApp';
+// import SlideToUnlock from './project/slide-to-unlock/SlideToUnlock';
+// import LightDarkMode from './project/lightDarkMode/LightDarkMode';
+// import TempControle from './project/temperature-app/TempControle';
+// import AlertApp from './project/Alerts-App/AlertApp';
+// import LikePhoto from './project/likePhotoApp/LikePhoto';
+// import TestimonialApp from './project/testimonial-app/TestimonialApp';
+// import RandomColor from './project/randomizeColor.app/RandomColor';
+// import EsignatureApp from './project/signature.app/EsignatureApp';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <EsignatureApp/> */}
+      {/* <RandomColor/> */}
+      {/* <LikePhoto/> */}
+      {/* <TestimonialApp/> */}
+      {/* <AlertApp/> */}
+      {/* <TempControle/> */}
+      {/* <LightDarkMode/> */}
+      {/* <SlideToUnlock/> */}
+      {/* <SliderApp /> */}
+      {/* <PopupApp/> */}
+      {/* <ProgressBarComp/> */}
+      {/* <DialogBox/> */}
+      {/* <FilterContact/> */}
+      {/* <RegForm/> */}
+      {/* <NotesApp/> */}
+      {/* <ResponsiveNav/> */}
+      <TextAnimationApp/>
+    </div>
+  );
+}
+
+export default App;
